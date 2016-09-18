59d1411952d87e1b160b8c76c05e015d6cd3fe18
Ascendzor <wow.tmare@gmail.com>
2016-09-19 09:21:49 +1200

    rendering off string stufferino

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 var queryString = require('query-string')
+var request = require('superagent')
+var _ = require('lodash')
 var queryStrings = queryString.parse(location.search)
 var jsonDatums = JSON.parse(atob(queryStrings.datums))
-console.log(jsonDatums)
+
+var setupLeaflet = require('./setupLeaflet')
+var leafletInterface = setupLeaflet()
+
+_.forEach(jsonDatums.layerIds, function (layerId) {
+  var wxTilesDotCom = 'https://api.wxtiles.com/'
+  request
+    .get(wxTilesDotCom + 'v0/wxtiles/layer/' + layerId)
+    .end((err, res) => {
+      var layer = res.body
+      var instances = _.sortBy(layer.instances, (instance) => { return instance.displayName }).reverse()
+      var recentestInstance = instances[0]
+
+      request
+        .get(wxTilesDotCom + 'v0/wxtiles/layer/' + layerId + '/instance/' + recentestInstance.id)
+        .end((err, res) => {
+          var times = res.body.times
+          var someRandomTime = times[0]
+
+          var url = layer.resources.tile.replace('<instance>', recentestInstance.id).replace('<time>', someRandomTime)
+          console.log(url)
+          url = wxTilesDotCom + 'v0/' + url
+          leafletInterface.addLayer(url)
+        })
+    })
+})
---
+++ setupLeaflet.js
@@ -0,0 +1,22 @@
+var leaflet = require('leaflet')
+
+module.exports = function (options) {
+  var leafletMap = leaflet.map('leafletMap', {
+    zoom: 5
+  }).setView([-20, 160], 2)
+
+  var baseMap = leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(leafletMap)
+
+  var addLayer = function(url) {
+    var leafletMapLayer = leaflet.tileLayer(url, {
+      tms: true
+    });
+    leafletMapLayer.addTo(leafletMap);
+  }
+
+  return {
+    addLayer: addLayer
+  }
+}
