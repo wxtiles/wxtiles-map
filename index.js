2207297afb964158f113f060059c1e7ba8087641
Ascendzor <wow.tmare@gmail.com>
2016-09-22 17:26:22 +1200

    using url now instead of resources

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var request = require('superagent')
 var _ = require('lodash')
 var jsongString = atob(window.location.href.split('?datums=')[1])
 var jsonDatums = JSON.parse(jsongString)
+var wxtilesjs = require('./mapOverlay/wxtiles')
 
 var setupLeaflet = require('./setupLeaflet')
 var leafletInterface = setupLeaflet({
@@ -25,25 +26,34 @@ _.forEach(jsonDatums.mapDatums.layers, (mapDatumsLayer) => {
         .get(wxTilesDotCom + 'v0/wxtiles/layer/' + layer.id + '/instance/' + recentestInstance.id)
         .end((err, res) => {
           var times = res.body.times
-          var someRandomTime = times[0]
+          var someRandomTime = times[2]
 
-          var url = layer.resources.tile.replace('<instance>', recentestInstance.id).replace('<time>', someRandomTime)
-          url = wxTilesDotCom + 'v0' + url
-          leafletInterface.addLayer({
-            url,
-            opacity: mapDatumsLayer.opacity
-          })
-
-          var layerHandle = _.find(jsonDatums.mapDatums.layers, (findingLayer) => findingLayer.id == layer.id)
-          layerHandle.instanceId = recentestInstance.id
-          layerHandle.label = layer.meta.name
-
-          var mapOverlayMount = document.querySelector('#mapOverlayMount')
-          ReactDOM.render(React.createElement('div', {className: 'mapControlsContainer'},
-            React.createElement(mapOverlay, {
-              layers: jsonDatums.mapDatums.layers
+          var withUrl = (url) => {
+            leafletInterface.addLayer({
+              url,
+              opacity: mapDatumsLayer.opacity
             })
-          ), mapOverlayMount)
+
+            var layerHandle = _.find(jsonDatums.mapDatums.layers, (findingLayer) => findingLayer.id == layer.id)
+            layerHandle.instanceId = recentestInstance.id
+            layerHandle.label = layer.meta.name
+
+            var mapOverlayMount = document.querySelector('#mapOverlayMount')
+            ReactDOM.render(React.createElement('div', {className: 'mapControlsContainer'},
+              React.createElement(mapOverlay, {
+                layers: jsonDatums.mapDatums.layers
+              })
+            ), mapOverlayMount)
+          }
+
+          wxtilesjs.getTileLayerUrl({
+            layerId: layer.id,
+            instanceId: recentestInstance.id,
+            time: someRandomTime,
+            level: 0,
+            onSuccess: withUrl,
+            onError: (err) => console.log(err)
+          })
         })
     })
 })
