632a15cbabbee3b3814d4a42310f821778ad57c2
Ascendzor <wow.tmare@gmail.com>
2016-09-29 12:22:33 +1300

    fixed key issue

--- mapWrapper.js
+++ mapWrapper.js
@@ -12,14 +12,31 @@ class mapWrapper extends React.Component {
   }
 
   render() {
-    console.log(this.props)
     var mapParams = {
       className: 'map',
       center: this.props.center,
       zoom: this.props.zoom
     }
+    var layers = this.props.layers
+    var tileLayersProps = _.map(layers, (layer) => {
+      return _.map(layer.timeUrlsToRender, (timeUrl) => {
+        var opacity = 0
+        if (timeUrl.isVisible) opacity = layer.opacity
+        return {
+          zIndex: layer.zIndex,
+          opacity: opacity,
+          url: timeUrl.url,
+          key: layer.zIndex + ' ' + timeUrl.url,
+          tms: true
+        }
+      })
+    })
+    tileLayersProps = _.flatten(tileLayersProps)
     return React.createElement('div', {className: 'mapWrapper'},
       React.createElement(reactLeaflet.Map, mapParams,
+        _.map(tileLayersProps, (tileLayerProps) => {
+          return React.createElement(reactLeaflet.TileLayer, tileLayerProps)
+        }),
         React.createElement(reactLeaflet.TileLayer, {
           url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
           subdomains: 'abcd'
