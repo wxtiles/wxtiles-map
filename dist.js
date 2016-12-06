55d17030d5131f0edda2b106b76e3210d1776735
alpha-beta-soup <richard.m.law@gmail.com>
2016-12-07 09:54:26 +1300

    removes tolerance time for timeslider that caused on offset bug

--- dist.js
+++ dist.js
@@ -69224,17 +69224,17 @@ var calculateLayerBufferUsingTime = (mapOptions) => {
   var lowerBufferLength = 5
   var buffer = []
   _.forEach(mapOptions.layers, (layer) => {
-    var afterSelectedTime = _.filter(layer.timeUrls, (timeUrl, key) => {
-      return timeUrl.time.isSameOrAfter(mapTime.clone().add(30, 'minutes'))
+    var atOrAfterMapTime = _.filter(layer.timeUrls, (timeUrl, key) => {
+      return timeUrl.time.isSameOrAfter(mapTime)
     }).slice(0, bufferLength)
-    if (afterSelectedTime.length == 0) {
+    if (atOrAfterMapTime.length == 0) {
       // Current time is beyond upper range of layer
       // Skip; we first need check if it's within tolerance time at upper edge
       // before giving a layer nothing to render
-    } else if (!mapTime.isSame(_.first(afterSelectedTime).time)) {
+    } else if (!mapTime.isSame(_.first(atOrAfterMapTime).time)) {
       // We're actually working with durations
       // Preprend the last time that is before the current time
-      afterSelectedTime.unshift(layer.timeUrls[layer.timeUrls.indexOf(afterSelectedTime[0])-1])
+      atOrAfterMapTime.unshift(layer.timeUrls[layer.timeUrls.indexOf(atOrAfterMapTime[0])-1])
     }
     var beforeSelectedTime = _.filter(layer.timeUrls, (timeUrl) => {
       return timeUrl.time.isSameOrBefore(mapTime)
@@ -69244,7 +69244,7 @@ var calculateLayerBufferUsingTime = (mapOptions) => {
       layer.timeUrlsToRender = []
       return
     }
-    if ((beforeSelectedTime.length > 0) && (afterSelectedTime.length == 0)) {
+    if ((beforeSelectedTime.length > 0) && (atOrAfterMapTime.length == 0)) {
       // mapTime is to the right of layer's time extent;
       // We want to continue showing the last time for a short period,
       // otherwise it will never be seen, even if it's the most recent obs
@@ -69253,21 +69253,21 @@ var calculateLayerBufferUsingTime = (mapOptions) => {
       var lastTimeRange = beforeSelectedTime.length > 1 ? end.diff(start, 'minutes') : 60
       var validUntil = end.add(lastTimeRange, 'minutes')
       if (mapTime.diff(validUntil, 'minutes') <= lastTimeRange) {
-        afterSelectedTime = beforeSelectedTime.slice(-1)
+        atOrAfterMapTime = beforeSelectedTime.slice(-1)
         beforeSelectedTime = []
       } else {
         layer.timeUrlsToRender = []
         return
       }
-    } else if (afterSelectedTime.length == 0) {
+    } else if (atOrAfterMapTime.length == 0) {
       layer.timeUrlsToRender = []
       return
     }
-    if (afterSelectedTime.length < bufferLength) {
+    if (atOrAfterMapTime.length < bufferLength) {
       // Get elements from the front of the array (facilitates looping)
-      buffer = afterSelectedTime.concat(beforeSelectedTime.slice(0, Math.min(lowerBufferLength, bufferLength - afterSelectedTime.length)))
+      buffer = atOrAfterMapTime.concat(beforeSelectedTime.slice(0, Math.min(lowerBufferLength, bufferLength - atOrAfterMapTime.length)))
     } else {
-      buffer = afterSelectedTime
+      buffer = atOrAfterMapTime
     }
     layer.timeUrlsToRender = _.flatten(buffer.filter(Boolean))
     _.forEach(layer.timeUrlsToRender, (timeUrl, key) => {
